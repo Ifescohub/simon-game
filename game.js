a026d26eb17733a8cdc54a1f686f3a24933e05cf
Ifescohub <101941367+Ifescohub@users.noreply.github.com>
2022-06-28 16:54:30 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,88 +1,93 @@
 
-let buttonColors = ["red", "blue", "green", "yellow"];
+//storing the button colors
+let buttonColors = [ "red", "blue", "green", "yellow"];
 
+// Storing the game pattern and the user's click pattern 
 let gamePattern = [];
 let userClickedPattern = [];
 
-
-let start = false;
+//A check for when the game as started
+let start = true;
 let level = 0;
 
-
+// Formatting Keyboard press to start the game only
 $(document).keydown(function(){
-    if (!start){
-        $("h1").text("level "+level);
+    if (start) {
+        $("h1").html("Level "+level);
         nextSequence();
-        start = true;
-    }  
+        start = false;
+    }   
 });
 
-
-
-$(".btn").click(function(){
+//Fomatting Button Clicks
+$(".box").click(function(){
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    animate(userChosenColour);
     playSound(userChosenColour);
-    animatePress(userChosenColour);
     checkAnswer(userClickedPattern.length-1);
 });
 
-
-
-
-
+// formatting the sewuence of the game when played
 function nextSequence(){
-    userClickedPattern= [];
+    userClickedPattern = [];
     level++;
-    $("h1").text("Level " + level);
+    $("h1").html("Level "+level);
+
     let randomNumber = Math.floor(Math.random()*4);
-    let  randomChosenColour  = buttonColors[randomNumber];
+    let randomChosenColour = buttonColors[randomNumber];
     gamePattern.push(randomChosenColour);
-    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#"+randomChosenColour).delay(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
 
-
+//creating the function that checks the game pattern against user's click and call the next sequence
 function checkAnswer(currentLevel){
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
-        if (userClickedPattern.length === gamePattern.length){
-            setTimeout(function(){
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
                 nextSequence();
             }, 1000);
         }
-        
-    } else{
+    }else{
         playSound("wrong");
         $("body").addClass("game-over");
-        $("h1").text("Game Over, Press Any Key to Restart");
-        setTimeout(function () {
+        setTimeout(() => {
             $("body").removeClass("game-over");
-          }, 200);
+        }, 200);
+        $("h1").html("Game Over, Press Any Key to Restart");
 
-          startOver();
+        startOver();
     }
 }
 
-
-
-
-
-
-function playSound(name){
-    let sound = new Audio("sounds/"+name+".mp3");
+// storing sounds to be played in the game
+function playSound(key){
+    let sound = new Audio("sounds/"+key+".mp3");
     sound.play();
 }
 
-function animatePress(currentColour){
-    $("#"+currentColour).addClass("pressed");
-    setTimeout( function(){
-        $("#"+currentColour).removeClass("pressed");
+//Formatting animation for the button press
+function animate(button){
+    $("#"+button).addClass("pressed");
+    setTimeout(() => {
+        $("#"+button).removeClass("pressed");
     }, 100);
 }
 
-
-function startOver() {
+//What happens when user gets answer wrong = start over
+function startOver(){
     level = 0;
     gamePattern = [];
-    start = false;
-  }
+    start = true;
+}
+
+
+// To display how it Works when "help" button is clicked
+$("button").click(function(){
+    $(".explainer").toggleClass("hidden");
+});
+
+$("button").hover(function(){
+    $(".explainer").toggleClass("hidden");
+});
